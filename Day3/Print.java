ee3e0f6b901602e9cba85688528447a0901a4e1e
Kim-wonkyu <83902559+Kimwonkyu-korea@users.noreply.github.com>
2021-08-25 20:37:22 +0900

    Study_0825

--- Day3/Print.java
+++ Day3/Print.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class MainClass {
+public class Print {
 
 	public static void main(String[] args) {
 		
