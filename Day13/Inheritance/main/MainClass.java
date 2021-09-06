632d8227d323c9f05aa88b42cbfd06c208d46bfb
Kim-wonkyu <83902559+Kimwonkyu-korea@users.noreply.github.com>
2021-09-06 22:08:44 +0900

    Update MainClass.java

--- Day13/Inheritance/main/MainClass.java
+++ Day13/Inheritance/main/MainClass.java
@@ -1,17 +1,30 @@
 package main;
 
-public class MainClass {
+import cls.ChildClass;
+import cls.ParentsClass;
+
+public class Main {
 
 	public static void main(String[] args) {
-		
 		/*
-		 	Inheritance : 상속
-		 	
-		 	부모 클래스 -> 자식 클래스
-		 	variable(변수)
-		 	method(함수)
-		 	
-		 */
+		   inheritance : 상속
+		   
+		   부모클래스 -> 자식 클래스(부모클래스로부터 상속을 받고싶음)
+		   variable(변수)
+		   method(함수) -> 둘다 상속
+		*/
+		
+	//	ParentsClass pc = new ParentsClass("홍길동", 1001);
+	//	pc.pMethod();
+		
+		ChildClass cc = new ChildClass();
+		cc.pMethod();
+		
+	//	cc.number = 123;
+		cc.age = 12;
+		
+		ChildClass cc1 =  new ChildClass("김철수", 1002, " 서울시", 25);
+
 	}
 
 }
