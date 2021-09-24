43e7dbf2122303763dfdf4f4d127471ec028b4f4
Kim-wonkyu <dnjsrb7617@naver.com>
2021-09-24 20:56:04 +0900

    Study_0924

---
+++ Day26/jsSample1/WebContent/myJS.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+function inputBtn() {
+	let num = document.getElementById("num").value;
+	
+	document.getElementById("out").value = parseInt(num) * 2;
+	
+	// String -> Integer = Integer.parseInt("123");
+}
