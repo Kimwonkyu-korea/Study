55d18d131814cd99cffe954d0e0313e3ebc3e525
wonkyu <dnjsrb7617@naver.com>
2021-11-23 23:24:54 +0900

    aws

---
+++ Day56/sprBootSample2MySql/sprBootSample2MySql/src/main/java/mul/com/a/SprBootSample2MySqlApplication.java
@@ -0,0 +1,13 @@
+package mul.com.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprBootSample2MySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprBootSample2MySqlApplication.class, args);
+	}
+
+}
